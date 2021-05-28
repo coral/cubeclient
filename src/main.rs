828aea3781b462c6bfb1249225479bd9062bc831
coral <jonas@jonasbengtson.se>
2021-05-27 21:58:12 -0700

    ok

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use tokio;
+
+use std::error::Error;
+use std::io;
+use tokio::{io::AsyncRead, io::AsyncReadExt, net::TcpListener, net::TcpStream, task};
+mod util;
+
+async fn process_socket(mut stream: tokio::net::TcpStream, addr: std::net::SocketAddr) {
+    loop {
+        let channel = stream.read_u8().await.unwrap();
+        let command = stream.read_u8().await.unwrap();
+        let size = stream.read_u16().await.unwrap();
+        let mut data = vec![0u8; size as usize];
+        stream.read_exact(&mut data).await;
+
+        let m: Vec<util::Color> = data
+            .chunks_exact(3)
+            .map(|v| util::Color {
+                r: v[0],
+                g: v[1],
+                b: v[2],
+            })
+            .collect();
+
+        dbg!(m);
+    }
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:7890").await?;
+
+    loop {
+        match listener.accept().await {
+            Ok((socket, addr)) => process_socket(socket, addr).await,
+            Err(e) => println!("couldn't get client: {:?}", e),
+        }
+    }
+}
---
+++ src/util.rs
@@ -0,0 +1,6 @@
+#[derive(Debug)]
+pub struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8,
+}
